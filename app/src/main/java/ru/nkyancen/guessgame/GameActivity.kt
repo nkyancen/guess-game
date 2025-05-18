7bed0a99375745146f069c60304780a5ea7a2217
Nikita Yantsen <nkyancen@yandex.ru>
2025-05-18 18:56:21 +0300

    Create base structure, add resource files and create icon

--- app/src/main/java/ru/nkyancen/guessgame/GameActivity.kt
+++ app/src/main/java/ru/nkyancen/guessgame/GameActivity.kt
@@ -14,7 +14,7 @@ class GameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
---
+++ app/src/main/java/ru/nkyancen/guessgame/utils/Game.kt
@@ -0,0 +1,8 @@
+package ru.nkyancen.guessgame.utils
+
+class Game(
+    val attemptsAmount: Int = 10,
+    val downLimit: Int = 0,
+    val upperLimit: Int = 1000
+) {
+}
